b7e7be128dd63c48087b7bc64f08f3895a097794
Le Huynh Duc <lehuynhducndyd@gmail.com>
2024-05-03 15:07:21 +0700

    ghep file

--- js/javascript.js
+++ js/javascript.js
@@ -47,3 +47,96 @@ function ktdnad(f){
     alert("Tên đăng nhập hoặc mật khẩu không chính xác")
     return false
 }
+
+function thongbao1(){
+    alert("Chức năng đang được phát triển")
+}
+
+function kt(){
+    var f =document.getElementById('RegistForm');
+    var emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,4}$/;
+    if(emailReg.test(f.email.value) == false){
+        document.getElementById('emailInvalid').innerText = '* Email không hợp lệ'; 
+        document.getElementById('passInvalid').innerText = '';
+        document.getElementById('rePassInvalid').innerText = '';
+        f.email.focus();
+        return false;
+    }
+    var passReg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g;
+    if(passReg.test(f.pwd.value)==false){
+        document.getElementById('passInvalid').innerText = '* Mật khẩu không hợp lệ';
+        document.getElementById('emailInvalid').innerText ='';
+        document.getElementById('rePassInvalid').innerText = '';
+        f.pwd.focus();
+        return false;
+    }
+    if(f.repwd.value != f.pwd.value){
+        document.getElementById('rePassInvalid').innerText = '* Mật khẩu chưa khớp';
+        document.getElementById('emailInvalid').innerText ='';
+        document.getElementById('passInvalid').innerText = '';
+        f.repwd.focus();
+        return false;
+    }
+
+    var email = f.email.value.trim();
+    var password = f.pwd.value.trim();
+
+    // Kiểm tra xem tên đăng nhập đã tồn tại hay chưa
+    const existingUsers = localStorage.getItem('registeredUsers') || '{}';
+    const parsedUsers = JSON.parse(existingUsers);
+
+    if (parsedUsers[email]) {
+        alert('Email đã được sử dụng!');
+        f.email.focus();
+        return false;
+    }
+
+    // Lưu thông tin đăng ký vào Local Storage
+    parsedUsers[email] = password;
+    localStorage.setItem('registeredUsers', JSON.stringify(parsedUsers));
+
+    alert('Đăng ký thành công!');
+    
+
+    // Xóa dữ liệu trong form
+
+    // var phoneReg =/^[+]{1}(?:[0-9\-\\(\\)\\/.]\s?){6,15}[0-9]{1}$/;
+    // if(phoneReg.test(f.pnumber.value) == false){
+    //     document.getElementById('phoneInvalid').innerText = '* Số điện thoại không hợp lệ';
+    //     f.pnumber.focus();
+    //     return false;
+    // }
+    // const emailDangNhap = document.getElementById('email').value;
+    // const matKhau = document.getElementById('pwd').value;
+
+    // // Lưu thông tin đăng ký vào LocalStorage
+    // localStorage.setItem('emailDangNhap', emailDangNhap);
+    // localStorage.setItem('matKhau', matKhau);
+    // alert('Đăng ký thành công!');
+    // window.location="index.html";
+
+};
+
+
+function DangNhap() {
+    const loginForm = document.getElementById('LoginForm');
+ 
+
+    const loginEmail = loginForm.email.value.trim();
+    const loginPassword = loginForm.pwd.value.trim();
+
+  // Retrieve registered users from Local Storage
+    var existingUsers = localStorage.getItem('registeredUsers') || '{}';
+    var parsedUsers = JSON.parse(existingUsers);
+
+  // Check if username and password match a registered user
+  if (parsedUsers[loginEmail] && parsedUsers[loginEmail] === loginPassword) {
+    alert('Đăng nhập thành công!');
+    window.location="index.html";
+    // Perform actions after successful login (e.g., redirect to user profile)
+  } else {
+    alert('Tên đăng nhập hoặc mật khẩu không chính xác!');
+    return false;
+  }
+
+};
